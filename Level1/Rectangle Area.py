afcd753fc4007ca96e5c77e1675335f683a6ae5c
M Hussain Khan <hussaink4235@gmail.com>
2026-06-26 21:56:10 +0500

    Rectangle area calculator
    Implement a Python script to calculate and display the area of a rectangle based on user input for length and width.

---
+++ Level1/Rectangle Area.py
@@ -0,0 +1,11 @@
+# Calculate the area of a rectangle using length and width.
+
+leng = int (input("Enter length:"))
+
+width = int (input("Enter width:"))
+
+area = (leng*width)
+
+print ("Total Area", area )
+
+print ("Thanks for using")
