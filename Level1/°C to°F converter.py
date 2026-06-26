bc679c1d0e35820d83e713e358df971e0a3ae426
M Hussain Khan <hussaink4235@gmail.com>
2026-06-26 21:43:14 +0500

    Add Celsius to Fahrenheit converter script

---
+++ Level1/°C to°F converter.py
@@ -0,0 +1,6 @@
+#Celsius to Fahrenheit Converter
+print ("--------Celsius to Fahrenheit Converter--------") 
+tem = int (input("Enter Celsius value:"))
+cal = (tem*9/5) + 32
+print (cal, "Fahrenheit")
+print ("Thanks for using")
