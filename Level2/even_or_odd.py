cd1e04d0d2df85ded2aca71b9a71a62d1d45a1cd
M Hussain Khan <hussaink4235@gmail.com>
2026-06-27 13:47:42 +0500

    Number checker is Even or Odd

---
+++ Level2/even_or_odd.py
@@ -0,0 +1,13 @@
+# Determine if a number is even or odd
+print ("<------Number check is EVEN OR ODD------>")
+
+print ("")
+
+num= int (input("Enter Number: "))
+
+if (num % 2 == 0):
+    print ("Number is Even")
+else:
+    print ("Number is Odd")
+    
+print ("Thanks for using")
