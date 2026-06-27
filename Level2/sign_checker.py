946a0dabd37e3b531bae00def7b5aa0bc9c9f3c1
M Hussain Khan <hussaink4235@gmail.com>
2026-06-27 13:31:53 +0500

    Implement sign checker for user input

---
+++ Level2/sign_checker.py
@@ -0,0 +1,23 @@
+
+# Check whether the number is positive, negative, or zero
+
+print ("<--------Check whether the number is positive, negative, or zero-------->")
+
+num = (int(input("Enter number:")))
+
+if num < 0:
+    print ("Number is Negative")
+
+    print ("Thanks for using")
+
+elif num > 0:
+
+    print ("Number is Positive")
+
+    print ("Thanks for using")
+
+else:
+
+    print ("Number is Zero")
+
+    print ("Thanks for using")
