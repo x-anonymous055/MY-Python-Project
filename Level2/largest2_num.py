b1650e771ea1f9bc806e695e63efdeffd19c4190
M Hussain Khan <hussaink4235@gmail.com>
2026-06-28 00:12:08 +0500

    Add script to find the largest of three numbers
    This script compares three user-input numbers and displays the largest one.

---
+++ Level2/largest2_num.py
@@ -0,0 +1,23 @@
+# Compare three numbers and display the larger one
+
+print ("<--------Find Largest Number-------->")
+
+print ("___________________")
+
+print ("")
+
+num1 = int (input ("Enter 1st Number:"))
+
+num2 = int (input("Enter 2nd Number:"))
+
+num3 = int (input("Enter 3rd Number:"))
+
+print ("___________________")
+
+print ("")
+
+print  ("(COMPARING ALL NUMBERS)" )
+
+print ("")
+
+print(f"Max number is: {max(num1, num2, num3)}")
