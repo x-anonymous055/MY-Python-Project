cfbf684f732b69dcd652e2e1916f1e4f7168d8fe
M Hussain Khan <hussaink4235@gmail.com>
2026-07-09 15:48:42 +0500

    Add fruit list loop exercise

---
+++ Level 5: Lists/fruit_list.py
@@ -0,0 +1,14 @@
+# Create a list of 5 fruits and print them using a (While) loop
+
+fruits = ['Apple', 'Mango', 'Banana', 'Orange']
+i = 0
+while i < len(fruits):
+    print (fruits[i])
+    i += 1
+  
+# Create a list of 5 fruits and print them using a (For)loop
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+for fruit in fruits:
+    print(fruit)
